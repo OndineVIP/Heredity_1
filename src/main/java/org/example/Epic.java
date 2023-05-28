4341d2aa9c094f156c025608dc01028e1d521d64
Ondine <ondinevika@gmail.com>
2023-05-28 15:12:29 +0300

    first commit

---
+++ src/main/java/org/example/Epic.java
@@ -0,0 +1,37 @@
+package org.example;
+
+
+public class Epic extends Task {
+    private String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public Epic(int id, String Молоко, String Яйца, String Хлеб) {
+        super(id);
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+
+    @Override
+    public boolean matches(String query) {
+        for (String subtask : subtasks) {
+            if (subtask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(String query) {
+        return false;
+
+    }
+}
+
---
+++ src/test/java/org/example/TaskTest.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+    @Test
+    public void shouldFoundSimpleTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        boolean actual = simpleTask.matches("родителям");
+        boolean expected = true;
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void shouldNotFoundSimpleTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        boolean actual = simpleTask.matches("подруге");
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouldFindTrueOrFalse() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Assertions.assertTrue(meeting.matches("приложения"));
+        Assertions.assertFalse(meeting.matches("прилажение"));
+        Assertions.assertTrue(meeting.matches("НетоБанка"));
+        Assertions.assertFalse(meeting.matches("Нетабанка"));
+        Assertions.assertFalse(meeting.matches("вторник"));
+        Assertions.assertTrue(meeting.matches("Выкатка"));
+        Assertions.assertFalse(meeting.matches("выкатика"));
+
+
+    }
+
+    @Test
+    public void shouldFindTrueOrFalse1() {
+        String[] subtasks = {"Молоко", "Яйца", "Яйца"};
+        Epic epic = new Epic(55, subtasks);
+        Epic epicEmpty = new Epic(55, new String[]{});
+
+
+        Assertions.assertTrue(epic.matches("Молоко"));
+        Assertions.assertTrue(epic.matches("Яйца"));
+        Assertions.assertFalse(epic.matches("Хлеб"));
+        Assertions.assertFalse(epic.matches("Хлеба"));
+        Assertions.assertFalse(epic.matches("сметана"));
+        Assertions.assertFalse(epic.matches("Малака"));
+    }
+}
+
+
